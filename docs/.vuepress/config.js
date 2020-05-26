d7448069a89974964a7fea7399ce8d5d0d624f0c
anjina <947937624@qq.com>
2020-05-26 08:12:49 +0800

    更新目录

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,12 +8,14 @@ module.exports = {
   markdown: {
     lineNumbers: true // 代码块显示行号
   },
+  lastUpdated: 'Last Updated',// 文档更新时间：每个文件git最后提交的时间,
+  sidebarDepth: 2, // 侧边栏显示2级
   themeConfig: {
     // [以 '/' 结尾的默认指向该路径下README.md文件]
     nav: [ // 导航栏配置
       { text: '前端基础', link: '/accumulate/' },
-      { text: '算法题库', link: '/algorithm/' },
-      { text: '微博', link: 'https://baidu.com' },
+      { text: '算法', link: '/algorithm/' },
+      { text: '数据结构', link: '/dataStructure/' },
       {
         text: 'GitHub',
         items: [
@@ -23,8 +25,114 @@ module.exports = {
         ]
       }
     ],
-    sidebar: 'auto', // 侧边栏配置
-    lastUpdated: 'Last Updated',// 文档更新时间：每个文件git最后提交的时间,
-    sidebarDepth: 4, // 侧边栏显示2级
+    sidebar: {
+      '/dataStructure/': [
+        '/dataStructure/',
+        {
+          title: '链表',
+          children: [
+            '/dataStructure/链表/链表',
+          ]
+        },
+        {
+          title: '字符串',
+          children: [
+            '/dataStructure/字符串/字符串',
+          ]
+        },
+        {
+          title: '栈和队列',
+          children: [
+            '/dataStructure/栈和队列/栈和队列',
+          ]
+        },
+        {
+          title: '数组',
+          children: [
+            '/dataStructure/数组/数组',
+          ]
+        },
+        {
+          title: '二叉树',
+          children: [
+            '/dataStructure/二叉树/二叉树',
+          ]
+        },
+        {
+          title: '堆',
+          children: [
+            '/dataStructure/堆/堆',
+          ]
+        },
+        {
+          title: '哈希表',
+          children: [
+            '/dataStructure/哈希表/哈希表',
+          ]
+        },
+      ],
+      '/algorithm/': [
+        '/algorithm/',
+        {
+          title: '排序',
+          children: [
+            '/algorithm/排序/复杂度',
+            '/algorithm/排序/排序',
+            '/algorithm/排序/插入排序',
+            '/algorithm/排序/堆排序',
+            '/algorithm/排序/归并排序',
+            '/algorithm/排序/快速排序',
+            '/algorithm/排序/冒泡排序',
+            '/algorithm/排序/选择排序',
+          ]
+        },
+        {
+          title: '分治',
+          children: [
+            '/algorithm/分治/分治',
+          ]
+        },
+        {
+          title: '数学运算',
+          children: [
+          ]
+        },
+        {
+          title: '查找',
+          children: [
+            '/algorithm/查找/查找',
+          ]
+        },
+        {
+          title: '递归和循环',
+          children: [
+            '/algorithm/递归和循环/递归',
+          ]
+        },
+        {
+          title: '回溯算法',
+          children: [
+            '/algorithm/回溯算法/回溯算法',
+          ]
+        },
+        {
+          title: '动态规划',
+          children: [
+            '/algorithm/动态规划/动态规划',
+          ]
+        },
+        {
+          title: '贪心算法',
+          children: [
+            '/algorithm/贪心算法/贪心算法',
+          ]
+        },
+        {
+          title: '原地置换',
+          children: [
+          ]
+        },
+      ],
+    }, // 侧边栏配置
   }
 };
--- docs/README.md
+++ docs/README.md
@@ -1,16 +1,16 @@
 ---
 home: true
 heroImage: /img/logo.jpg
-heroText: Wiki 1001
-tagline: Meet 1000 Books & Unit Them Into 1 Wiki
-actionText: Get Wiki →
+heroText: D.W's blog
+tagline: Step By Step
+actionText: Go →
 actionLink: /algorithm/
 features:
-- title: Wiki - 求索
-  details: 基于书签对知识点进行 整理，吸收，吐纳，归档。吾将上下而求索...
-- title: Store - 仓库
-  details: 展望云仓库而归纳整理，方便行事&培养习惯。鱼和熊掌我全都要...
-- title: Thought - 随笔
-  details: 活着，是一件最能带来满足感的事。细细琢磨吧，人生啊，有意思的很...
-footer: MIT Licensed | Copyright © 2019.01.11-present Mulander-J
+- title: JavaScript
+  details:  JavaScript进阶
+- title: 前端工程化
+  details: 构建工具，脚手架相关
+- title: 算法与数据结构
+  details: 数据结构与算法分类，刷题总结
+footer: MIT Licensed | Copyright © 2019.01.11-present D.W
 ---
